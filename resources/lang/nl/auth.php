6de734609ddecb48fd5176553e2be9a81a0a5199
Daan de Waard <daan.de.waard@hz.nl>
2020-04-19 15:09:42 +0200

    Add nl language files

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed'   => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
+    'throttle' => 'Te veel mislukte loginpogingen. Probeer het over :seconds seconden nogmaals.',
+];
