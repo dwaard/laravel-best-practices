108cb4a1250944497ca5e77090f0d00cb1f381f3
Daan de Waard <daan.de.waard@hz.nl>
2020-04-21 10:35:26 +0200

    Add very basic account management

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::resource('/users', 'UserController')->middleware('auth');
